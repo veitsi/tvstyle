c9bac97cf11db7a7feef38d2d19866c0b2af3b72
pydev <xtfkpi@gmail.com>
2016-07-27 15:51:26 +0300

    issues dragging comes back

--- static/templates.js
+++ static/templates.js
@@ -5,22 +5,31 @@ var templates = {
     'hearts': {
         items: 5
     },
-    'elegant':{
-        items:6
+    'elegant': {
+        items: 6
     },
-    'ink':{
-        items:5
+    'ink': {
+        items: 5
     }
 
 };
-    function loadTemplate(id) {
-        document.querySelector('#canvas')
-                .appendChild(document.importNode(
-                        document.querySelector('link[rel="import"]').import.querySelector('#' + id).content, true));
+function loadTemplate(id) {
+    document.querySelector('#canvas')
+        .appendChild(document.importNode(
+            document.querySelector('link[rel="import"]').import.querySelector('#' + id).content, true));
+    var i = 1;
+    for (i; i <= templates[id].items; i++) {
+        console.log("#" + i);
+        $(".item-"+i).droppable({drop: droppedIn});
+
+        //$("#" + i).attr('src', links[i]);
+        //$("#" + i).droppable({drop: droppedIn});
     }
 
+}
 
-    //        var link = document.querySelector('link[rel="import"]');
-    //        var template = document.querySelector('link[rel="import"]').import.querySelector('#ink');
-    //        var clone = document.importNode(template.content, true);
+
+//        var link = document.querySelector('link[rel="import"]');
+//        var template = document.querySelector('link[rel="import"]').import.querySelector('#ink');
+//        var clone = document.importNode(template.content, true);
 
